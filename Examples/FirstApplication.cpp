8c63d4dec616a1190169a51a1d87f89d73031a6c
Ari Rosen <105858911+ari-rosen@users.noreply.github.com>
2025-05-28 21:42:03 -0500

    Update FirstApplication.cpp

--- Examples/FirstApplication.cpp
+++ Examples/FirstApplication.cpp
@@ -1,3 +1,9 @@
+/*
+Example code for creating a blank application
+It is *nececary* to define what platform or the engine will not compile
+for macOS, use ROSE_PLATFORM_MACOS
+*/
+
 #define ROSE_PLATFORM_MACOS
 #include <Rose.hpp>
 
